fb36b02752fead4882515138f072a6802405a8d5
nicklzx <48320374+inekit@users.noreply.github.com>
2022-12-11 22:34:01 +0300

    init

--- src/index.js
+++ src/index.js
@@ -45,7 +45,7 @@ console.log("started");
         console.log(r);
       });
 
-    await bot.startWebhook(secretPath, tlsOptions, 8443);
+    await bot.startWebhook(secretPath, tlsOptions, 3000);
 
     console.log(await ctx.telegram.getWebhookInfo());
   } else {
